35090c5f40c4fd84a65277747bab529c932fd1e8
Alexander Petree <apetree1001@email.phoenix.edu>
2024-01-06 04:20:30 -0800

    Update .eslintrc.cjs

--- .eslintrc.cjs
+++ .eslintrc.cjs
@@ -1,15 +1,8 @@
-module.exports = {
-  "env": {
-    "browser": true,
-    "es2018": true,
-    "node": true
-  },
-  "extends": "eslint:recommended",
-  "parserOptions": {
-    "ecmaVersion": 2018,
-    "sourceType": "module"
-  },
-  "rules": {
-    "no-cond-assign": 0
-  }
+module.exports = 
+{
+ "env": { "browser": true,
+    "es2018": true,  "node": true  },
+  "extends": "eslint:recommended","parserOptions": {
+    "ecmaVersion": 2018,"sourceType": "module" },
+  "rules": {"no-cond-assign": 0 }
 }
